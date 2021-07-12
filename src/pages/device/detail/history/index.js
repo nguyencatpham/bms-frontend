73574ee3bd89510a5eb3c800bc238fc89c4761e8
Lê Thanh Hà <lethanhha@blueriver.local>
2021-07-13 01:29:38 +0700

    [BMS-85] Improve Device log page

--- src/pages/device/detail/history/index.js
+++ src/pages/device/detail/history/index.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import moment from 'moment'
 import { withRouter, useParams } from 'react-router-dom'
 import style from './style.module.scss'
+import { List } from 'antd'
 
 const mapStateToProps = ({ system, dispatch }) => {
   const { loading, detail, histories = [] } = system
@@ -13,6 +14,16 @@ const mapStateToProps = ({ system, dispatch }) => {
 
 const DefaultPage = ({ loading, detail, histories, dispatch }) => {
   const { id } = useParams()
+  let total = histories.length
+  const [pagination, setPagination] = useState({
+    current: 1,
+    pageSize: 10,
+    total: 0,
+    // showTotal: (total, range) => `${range[0]}-${range[1]} trên ${total} tài khoản`,
+  })
+  const onListChange = (pagination) => {
+    setPagination(pagination)
+  }
   useEffect(() => {
     dispatch({
       type: 'system/HISTORY',
@@ -20,31 +31,46 @@ const DefaultPage = ({ loading, detail, histories, dispatch }) => {
         id,
         start: 1,
         end: Math.floor(Date.now() / 1000),
-        // macAddress,
-        // unitId,
-        // stringId,
         priority: [3],
         limit: 1000,
-        isAsc: false
-      }
+        isAsc: false,
+      },
     })
   }, [id, dispatch])
+
+  useEffect(() => {
+    if (total !== pagination.total) {
+      setPagination({ ...pagination, total })
+    }
+  }, [total, pagination, setPagination])
+
   return (
-    <ul className='list-unstyled'>
-      {
-        histories.map(({ type, title, body, timestamp }) => (
-          <li key={timestamp} className={style.item}>
-            <div className={`${style.itemTime} mr-3`}>{moment(timestamp).format(TIME_FORMAT)}</div>
-            <div className={style.itemSeparator}>
-              <div className={`${style.donut} ${type === 'OFFLINE' ? style.danger : ''} mr-3`} />
+    <div>
+      <List
+        grid={{ gutter: 0, column: 1 }}
+        rowKey={(x) => x.id}
+        dataSource={histories}
+        pagination={{ ...pagination, showSizeChanger: true, onChange: onListChange }}
+        onChange={onListChange}
+        renderItem={({type, title, body, timestamp}) => {
+          return (
+            <div key={timestamp} className={style.item}>
+              <div className="d-flex my-3">
+                <div className={`${style.itemTime} mr-3`}>
+                  {moment(timestamp).format(TIME_FORMAT)}
+                </div>
+                <div className={style.itemSeparator}>
+                  <div
+                    className={`${style.donut} ${type === 'OFFLINE' ? style.danger : ''} mr-3`}
+                  />
+                </div>
+                <div>{title || body}</div>
+              </div>
             </div>
-            <div>
-              {title || body}
-            </div>
-          </li>
-        ))
-      }
-    </ul>
+          )
+        }}
+      />
+    </div>
   )
 }
 
