dc06d7628d82a29455de1c634e4391045026a0bb
Lê Thanh Hà <lethanhha@blueriver.local>
2021-07-16 00:04:32 +0700

    [BMS-92] Implement Customer change password page

--- src/pages/account/detail/change-password.js
+++ src/pages/account/detail/change-password.js
@@ -1,18 +1,84 @@
+import React from 'react'
 import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom'
+import { Input, Button, Form } from 'antd'
+import { Link } from 'react-router-dom'
+import style from '../style.scss'
 
-const mapStateToProps = ({ }) => {
-  
-}
+const mapStateToProps = ({ user, settings, dispatch, account }) => ({
+  dispatch,
+  user,
+  authProvider: settings.authProvider,
+  logo: settings.logo,
+  version: settings.version,
+  accountDetail: account.detail,
+})
 
-const ChangePassword = ({
-}) => {
+const ChangePassword = ({ dispatch, user, logo, version, accountDetail }) => {
+  const onFinish = (values) => {
+    const { password } = values
+    dispatch({
+      type: 'account/SET_PASSWORD',
+      payload: { id: accountDetail.id, password },
+    })
+  }
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo)
+  }
+
   return (
-    <>
-      <div className="change-password">
-        <p>Chưa phát hành</p>
-      </div>
-    </>
+    <div>
+      <Form
+        layout="vertical"
+        hideRequiredMark
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        className="mb-4"
+      >
+        <Form.Item
+          className=""
+          name="password"
+          label="Mật khẩu mới"
+          rules={[
+            {
+              required: true,
+              message: 'Vui lòng nhập mật khẩu!',
+            },
+          ]}
+        >
+          <Input.Password placeholder="******" visibilityToggle />
+        </Form.Item>
+        <Form.Item
+          className=""
+          name="confirm"
+          label="Nhập lại mật khẩu"
+          dependencies={['password']}
+          rules={[
+            {
+              required: true,
+              message: 'Vui lòng nhập lại mật khẩu!',
+            },
+            ({ getFieldValue }) => ({
+              validator(value) {
+                if (!value || getFieldValue('password') === getFieldValue('confirm')) {
+                  return Promise.resolve()
+                }
+                return Promise.reject(new Error('Mật khẩu không khớp!'))
+              },
+            }),
+          ]}
+        >
+          <Input.Password placeholder="******" visibilityToggle />
+        </Form.Item>
+        <div className="text-right fl-right btn-footer btn-group-footer">
+          <Button type="primary" style={{ marginLeft: 10 }} htmlType="submit">
+            <i className="i_save_36 ico25" />
+            <strong>Cập nhật</strong>
+          </Button>
+        </div>
+      </Form>
+    </div>
   )
 }
-export default withRouter(connect(mapStateToProps)(ChangePassword))
+
+export default connect(mapStateToProps)(ChangePassword)
