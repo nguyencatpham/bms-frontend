36004b515eedcdd0ff5bca2153c2dbefd19140d4
Lê Thanh Hà <lethanhha@blueriver.local>
2021-07-15 20:40:57 +0700

    [BMS-99] Improve the breadcrumb

--- src/@vb/components/Breadcrumbs2/index.js
+++ src/@vb/components/Breadcrumbs2/index.js
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from 'react'
-import { withRouter } from 'react-router-dom'
+import { withRouter, Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { reduce } from 'lodash'
 import { history } from 'index'
 import styles from './style.module.scss'
+import { Breadcrumb } from 'antd'
+import { Fragment } from 'react'
 
 const mapStateToProps = ({ menu }) => ({
-  menuData: menu.menuData
+  menuData: menu.menuData,
 })
 
 const Breadcrumbs2 = (props) => {
   const [breadcrumbs, setBreadcrumbs] = useState([])
   const {
     location: { pathname },
-    menuData = []
+    menuData = [],
   } = props
   useEffect(() => {
     setBreadcrumbs(() => getBreadcrumbs())
@@ -34,44 +36,73 @@ const Breadcrumbs2 = (props) => {
         }
         return result
       },
-      []
+      [],
     )
     return items.length > 0 ? items : [false]
   }
 
-  // const toUpper = (str) => str.replace(/\b\w/g, (l) => l.toUpperCase())
-
   const getBreadcrumbs = () => {
-    const [activeMenuItem] = getPath(menuData, pathname)
+    let [activeMenuItem] = getPath(menuData, pathname)
     const pathUrl = pathname.split('/')
 
-    if (activeMenuItem && pathUrl.length > 1) {
-      return pathUrl.map((item, index) => {
+    let title2, title3, href2, href3
+    if (pathUrl.length === 2) {
+      title2 = activeMenuItem.title
+      href2 = pathname
+    } else if (pathUrl.length === 3) {
+      title3 = pathUrl[2] === 'create' ? 'Tạo mới' : ''
+      href3 = pathname
+      const item = menuData.find((d) => d.url.includes(pathUrl[1]))
+      title2 = item.title
+      href2 = '/' + pathUrl[1]
+    } else if (pathUrl.length === 4) {
+      title3 = pathUrl[3] === 'update' ? 'Chỉnh sửa' : ''
+      href3 = pathname
+      const item = menuData.find((d) => d.url.includes(pathUrl[1]))
+      title2 = item.title
+      href2 = '/' + pathUrl[1]
+    }
+
+    if (pathUrl.length > 1) {
+      return pathUrl.map((x, index) => {
+        const items = []
         if (index === 0) {
           return null
         }
 
-        if (index === pathUrl.length - 1) {
-          return (
-            <li className={styles.breadcrumb} key={item}>
-              <strong className={`${styles.breadcrumbLink} ${styles.breadcrumbLink__current}`}>
-                {(activeMenuItem.title || '').toUpperCase()}
-              </strong>
-            </li>
+        if (index === 1) {
+          if (pathUrl.includes('dashboard')) {
+            return null
+          }
+          const item = (
+            <Breadcrumb.Item>
+              <Link to={href2}>{title2.toUpperCase()}</Link>
+            </Breadcrumb.Item>
           )
+          items.push(item)
         }
-        return (
-          <li className={styles.breadcrumb} key={item}>
-            {(item || '').toUpperCase()}
-          </li>
-        )
+
+        if (index === 2) {
+          const item = (
+            <Breadcrumb.Item>
+              <Link to={href3}>{title3.toUpperCase()}</Link>
+            </Breadcrumb.Item>
+          )
+          items.push(item)
+        }
+        return items
       })
     }
     return (
-      <li className={styles.breadcrumb}>
-        <strong className={styles.current}>{activeMenuItem.title}</strong>
-      </li>
+      <Breadcrumb.Item>
+        <Link to={pathname}>{activeMenuItem.title}</Link>
+      </Breadcrumb.Item>
     )
+  }
+
+  const isDashboard = () => {
+    const pathUrl = pathname.split('/')
+    return !pathUrl.includes('dashboard')
   }
 
   const isCreatable = () => {
@@ -79,11 +110,13 @@ const Breadcrumbs2 = (props) => {
     if (current.split('/').length !== 2) {
       return false
     }
-    if (current === '/' ||
+    if (
+      current === '/' ||
       current === '/dashboard' ||
       current.includes('create') ||
       current.includes('update') ||
-      current.includes('detail')
+      current.includes('detail') ||
+      current.includes('events')
     ) {
       return false
     }
@@ -92,72 +125,43 @@ const Breadcrumbs2 = (props) => {
   return (
     <div className={styles.subbar}>
       <ul className={`${styles.breadcrumbs} mr-4`}>
-        <li className={styles.breadcrumb}>
-          <a href='#' className={styles.breadcrumbLink}>
-            Trang chủ
-          </a>
-        </li>
-        {breadcrumbs}
-        {/* <li className={styles.breadcrumb}>
-          <a href="#" className={`${styles.breadcrumbLink} ${styles.breadcrumbLink__current}`}>
-            Dashboard
-          </a>
-        </li> */}
+        <Breadcrumb>
+          <Breadcrumb.Item>
+            <Link to="/dashboard">TRANG CHỦ</Link>
+          </Breadcrumb.Item>
+          {breadcrumbs}
+        </Breadcrumb>
       </ul>
       <div className={`${styles.divider} mr-4 d-none d-xl-block`} />
-      {/* <p className='color-gray-4 text-uppercase font-size-16 mb-0 mr-4 d-none d-xl-block'>
-        INV-00125
-      </p> */}
-      {isCreatable() &&
+      {isCreatable() && (
         <button
           style={{ marginRight: 10 }}
-          onClick={() => { history.push(`${window.location.pathname}/create`) }}
-          type='button'
-          className='btn btn-primary btn-with-addon  text-nowrap d-none d-md-block'
+          onClick={() => {
+            history.push(`${window.location.pathname}/create`)
+          }}
+          type="button"
+          className="btn btn-primary btn-with-addon  text-nowrap d-none d-md-block"
         >
-          <span className='btn-addon'>
-            <i className='btn-addon-icon fe fe-plus-circle' />
+          <span className="btn-addon">
+            <i className="btn-addon-icon fe fe-plus-circle" />
           </span>
-        Thêm mới
-        </button>}
-      <button
-        onClick={() => { history.goBack() }}
-        type='button'
-        className='btn btn-default btn-with-addon mr-auto text-nowrap d-none d-md-block'
-      >
-        <span className='btn-addon'>
-          <i className='btn-addon-icon fe fe-plus-circle' />
-        </span>
-        Trở về
-      </button>
-      {/* <div className={`${styles.amount} mr-3 ml-auto d-none d-sm-flex`}>
-        <p className={styles.amountText}>
-          Tháng này
-          <span className={styles.amountValue}>$251.12</span>
-        </p>
-        <div className={styles.amountGraph}>
-          <i className={styles.amountGraphItem} style={{ height: '80%' }} />
-          <i className={styles.amountGraphItem} style={{ height: '50%' }} />
-          <i className={styles.amountGraphItem} style={{ height: '70%' }} />
-          <i className={styles.amountGraphItem} style={{ height: '60%' }} />
-          <i className={styles.amountGraphItem} style={{ height: '50%' }} />
-          <i className={styles.amountGraphItem} style={{ height: '65%' }} />
-        </div>
-      </div> */}
-      {/* <div className={`${styles.amount} d-none d-sm-flex`}>
-        <p className={styles.amountText}>
-          Tháng trước
-          <span className={styles.amountValue}>$12,256.12</span>
-        </p>
-        <div className={styles.amountGraph}>
-          <i className={styles.amountGraphItem} style={{ height: '60%' }} />
-          <i className={styles.amountGraphItem} style={{ height: '65%' }} />
-          <i className={styles.amountGraphItem} style={{ height: '75%' }} />
-          <i className={styles.amountGraphItem} style={{ height: '55%' }} />
-          <i className={styles.amountGraphItem} style={{ height: '100%' }} />
-          <i className={styles.amountGraphItem} style={{ height: '85%' }} />
-        </div>
-      </div> */}
+          Thêm mới
+        </button>
+      )}
+      {isDashboard() && (
+        <button
+          onClick={() => {
+            history.goBack()
+          }}
+          type="button"
+          className="btn btn-default btn-with-addon mr-auto text-nowrap d-none d-md-block"
+        >
+          <span className="btn-addon">
+            <i className="btn-addon-icon fe fe-plus-circle" />
+          </span>
+          Trở về
+        </button>
+      )}
     </div>
   )
 }
